f857d91551f0f4ab2046a95577049c6bee39e530
Laurent Gatto <lgatto@protonmail.ch>
2026-07-14 09:24:17 +0200

    update renv.lock

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.1.4"
-  attr(version, "md5") <- NULL
+  version <- "1.2.3"
+  attr(version, "md5") <- "1bd9f58e1cfe27ce035933937c6f03de"
   attr(version, "sha") <- NULL
 
   # the project directory
